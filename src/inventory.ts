bb2b84e1dfe219fbd3a62b7066adfe67eecb1298
KatSchultz <ks.katieschultz@gmail.com>
2022-06-23 10:37:13 -0400

    completed src files

--- src/inventory.ts
+++ src/inventory.ts
@@ -12,9 +12,22 @@ const inventory: InventoryItem[] = [
   },
   { product: { name: "sensor", price: 12.5 }, quantity: 4 },
   {
-    product: { name: "LED", price: 100 },
+    product: { name: "LED", price: 1.0 },
     quantity: 20,
   },
 ];
 
-function calcInventoryValue(arr: InventoryItem[]) {}
+function calcInventoryValue(arr: InventoryItem[]) {
+  const value = arr.reduce((acc, elem) => {
+    console.log(
+      `price: ${elem.product.price} * quantity: ${elem.quantity} = ${
+        elem.product.price * elem.quantity
+      }`
+    );
+    return acc + elem.product.price * elem.quantity;
+  }, 0);
+  console.log("value", value);
+  return value;
+}
+
+calcInventoryValue(inventory);
